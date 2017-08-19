681086cebe40a71e3b56fc2128ba5cd4a5ee4462
CrazyGamerGR <frankalexb97@gmail.com>
2017-08-20 01:58:03 +0300

    update build_anykernel.sh

--- build_anykernel.sh
+++ build_anykernel.sh
@@ -8,11 +8,11 @@
 #   		\_______|    |__|\___|
 ##########################################
 export ARCH=arm64
-export CROSS_COMPILE=/home/francesco/aarch64-linux-android-4.9-kernel/bin/aarch64-linux-android-
+export CROSS_COMPILE=/home/francesco/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 ##########################################
 mkdir output
 ##########################################
-make -C $(pwd) O=output cyanogenmod_pme_defconfig
-make -C $(pwd) O=output
+make -C $(pwd) O=output pme_defconfig
+make -j64 -C $(pwd) O=output
 ##########################################
 cp output/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
